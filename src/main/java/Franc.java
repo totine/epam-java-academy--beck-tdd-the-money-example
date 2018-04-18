3dbc13536724d8bc014d2e744648673d3cc74152
totine <joanna.gargas@gmail.com>
2018-04-18 11:50:28 +0200

    Change times() in Franc to use Franc instead of Money (tests passed)

--- src/main/java/Franc.java
+++ src/main/java/Franc.java
@@ -6,7 +6,7 @@ public class Franc extends Money {
     }
 
     Money times(int multiplier) {
-        return new Money(amount * multiplier, currency);
+        return new Franc(amount * multiplier, currency);
     }
 
 }
