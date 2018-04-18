e6be2c190c508447232e4ca6954ba371c03b24d9
totine <joanna.gargas@gmail.com>
2018-04-18 14:25:54 +0200

    Add addRate method in Bank without implementation (tests fail)

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -3,4 +3,9 @@ public class Bank {
     Money reduce(Expression source, String to) {
          return source.reduce(to);
     }
+
+    public void addRate(String from, String to, int rate) {
+        
+
+    }
 }
