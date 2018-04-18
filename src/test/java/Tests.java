302378376d483984e079f58a839d6c8a29983f4a
totine <joanna.gargas@gmail.com>
2018-04-18 14:21:35 +0200

    Add test for reduce money with different currencies (compilation error)

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -63,4 +63,12 @@ class Tests {
         assertEquals(Money.dollar(1), result);
     }
 
+    @Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
 }
