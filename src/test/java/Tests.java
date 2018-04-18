076b284915bc5c2f77a0160c4a985aa6b81ee8cc
totine <joanna.gargas@gmail.com>
2018-04-18 12:08:17 +0200

    Remove unnecessary tests and Dollar and Franc classes (tests passed)

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -17,24 +17,10 @@ class Tests {
     }
 
     @Test
-    public void testFrancMultiplication() {
-        //given
-        Franc five = Money.franc(5);
-        //when
-        //then
-        assertEquals(Money.franc(10), five.times(2));
-        //when2
-        //then2
-        assertEquals(Money.franc(15), five.times(3));
-    }
-
-    @Test
     public void testEquality() {
         // then
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
@@ -44,9 +30,5 @@ class Tests {
         assertEquals("CHF", Money.franc(1).currency());
     }
 
-    @Test
-    public void testDifferentClassEquality() {
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
-    }
 
 }
