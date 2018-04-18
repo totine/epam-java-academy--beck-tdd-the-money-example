d1cf040356d2ea9c80dcaebdd3651ac005611ece
totine <joanna.gargas@gmail.com>
2018-04-18 11:46:44 +0200

    Add toString() method in Money

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -30,4 +30,8 @@ public class Money {
         return currency;
     }
 
+    public String toString() {
+        return new StringBuilder().append(amount).append(" ").append(currency).toString();
+    }
+
 }
