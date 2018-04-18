ea74eec41a10c408676d0def0ed07fea0f127a9c
totine <joanna.gargas@gmail.com>
2018-04-18 11:02:49 +0200

    Change Money class to abstract, add factory method for Dollar and abstract times method

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -8,4 +8,6 @@ public class Dollar extends Money {
         return new Dollar(amount * multiplier);
     }
 
+
+
 }
--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,10 +1,16 @@
-public class Money {
+public abstract class Money {
 
     protected int amount;
+
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
     }
+
+    abstract Money times(int multiplier);
 
 }
