049c9dcd27f7808cec48ef8e34abf0afd6216e11
totine <joanna.gargas@gmail.com>
2018-04-18 11:13:56 +0200

    Add test for currency (failed)

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -38,4 +38,10 @@ class Tests {
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
+    @Test
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
+
 }
