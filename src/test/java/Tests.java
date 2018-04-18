44b61682af4f5722c6427151e3ff8bee405af0c3
totine <joanna.gargas@gmail.com>
2018-04-18 14:01:52 +0200

    Add test for reduce 1 dollar to "USD" (tests fail)

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -56,4 +56,11 @@ class Tests {
         assertEquals(Money.dollar(7), result);
     }
 
+    @Test
+    public void testReduceMoney() {
+        Bank bank = new Bank();
+        Money result = bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
 }
