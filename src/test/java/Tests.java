29cc2af855810b78b7f8b390eda9163a2913668b
totine <joanna.gargas@gmail.com>
2018-04-18 16:37:37 +0200

    Add test plus same currency returns Money (test fails)

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -103,4 +103,10 @@ class Tests {
         assertEquals(Money.dollar(20), result);
     }
 
+    @Test
+    public void testPlusSameCurrencyReturnsMoney() {
+        Expression sum = Money.dollar(1).plus(Money.dollar(1));
+        assertTrue(sum instanceof Money);
+    }
+
 }
