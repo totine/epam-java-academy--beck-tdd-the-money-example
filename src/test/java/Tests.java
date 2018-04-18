3811d4e421fd4ae352ccba6e291917f49b29686a
totine <joanna.gargas@gmail.com>
2018-04-18 16:38:11 +0200

    Remove worthless test

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -103,10 +103,4 @@ class Tests {
         assertEquals(Money.dollar(20), result);
     }
 
-    @Test
-    public void testPlusSameCurrencyReturnsMoney() {
-        Expression sum = Money.dollar(1).plus(Money.dollar(1));
-        assertTrue(sum instanceof Money);
-    }
-
 }
