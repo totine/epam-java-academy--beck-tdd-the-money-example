919cc7e8ede039c9804ebac33a7692796e63d7d9
totine <joanna.gargas@gmail.com>
2018-04-18 16:12:57 +0200

    Add test for mixed addition (tests fail)

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -71,4 +71,14 @@ class Tests {
         assertEquals(Money.dollar(1), result);
     }
 
+    @Test
+    public void testMixedAddition() {
+        Money fiveBucks = Money.dollar(5);
+        Money tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
+    }
+
 }
